0f0b4794bf19c3fb4d76de07ce601b26a048f3e2
t.kolpakova <t.kolpakova@tinkoff.ru>
2018-10-03 19:12:02 +0300

    fix npe

--- src/main/java/ru/edu/concur/servlets/FactorServlet.java
+++ src/main/java/ru/edu/concur/servlets/FactorServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,30 +14,26 @@ public class FactorServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        BigInteger num = extractFromRequest(req);
-        if (num == null) {
+        String numStr = req.getParameter("num");
+        if (numStr == null) {
             resp.sendRedirect("error.jsp");
             return;
         }
-        List<BigInteger> factors = factor(num);
+        Integer num = Integer.valueOf(numStr);
+        List<Integer> factors = factor(num);
         encodeIntoResponse(resp, req, factors);
     }
 
-    private void encodeIntoResponse(HttpServletResponse resp, HttpServletRequest req, List<BigInteger> factors) throws ServletException, IOException {
+    private void encodeIntoResponse(HttpServletResponse resp, HttpServletRequest req, List<Integer> factors) throws ServletException, IOException {
         req.setAttribute("countArray", factors);
         req.getRequestDispatcher("page.jsp").forward(req, resp);
     }
 
-    private BigInteger extractFromRequest(HttpServletRequest req) {
-        String num = req.getParameter("num");
-        return new BigInteger(num);
-    }
-
-    private List<BigInteger> factor(BigInteger i) {
-        List<BigInteger> bigIntegers = new ArrayList<>();
-        for (BigInteger j = i; j.compareTo(BigInteger.ZERO) > 0; j.subtract(BigInteger.ONE)) {
-            bigIntegers.add(j);
+    private List<Integer> factor(int i) {
+        List<Integer> res = new ArrayList<>();
+        for (int j = i; j > 0; j--) {
+            res.add(j);
         }
-        return bigIntegers;
+        return res;
     }
 }
